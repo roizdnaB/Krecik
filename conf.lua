b22e20eafc3a7611cb5b598a05f1021b126cc935
roizdnaB <danieljambor1999@gmail.com>
2020-11-08 17:15:40 +0100

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,15 @@
+--File: conf.lua
+
+--Configuration function
+function love.conf(t)
+  
+    --Set size of the window
+    t.window.width = 600
+    t.window.height = 600
+    
+    --Set title of the window
+    t.window.title = "Krecik"
+    
+    --Set the icon of the game
+    t.window.icon = "assets/krecik_icon.jpg"
+end
---
+++ main.lua
@@ -0,0 +1,18 @@
+--File: main.lua
+
+--Load libraries
+tiny = require "lib.tiny"       --Tiny for support ECS architectral pattern
+Object = require "lib.classic"  --Classic for simulating OOP
+
+--Main loading function of the game
+function love.load()
+end
+
+--Main drawing function of the game
+function love.draw()
+end
+
+--Main updating function of the game
+function love.update()
+end
+
