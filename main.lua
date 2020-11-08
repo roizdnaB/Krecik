ddd891bb0090ae4daeb474551cd9f89b8bdb9a2b
roizdnaB <danieljambor1999@gmail.com>
2020-11-08 17:45:05 +0100

    Added assets support

--- main.lua
+++ main.lua
@@ -4,8 +4,13 @@
 tiny = require "lib.tiny"       --Tiny for support ECS architectral pattern
 Object = require "lib.classic"  --Classic for simulating OOP
 
+local assets = nil
+
 --Main loading function of the game
 function love.load()
+  
+  --Load assets
+  assets = require "src.assets"
 end
 
 --Main drawing function of the game
---
+++ src/assets.lua
@@ -0,0 +1,10 @@
+--File: assets.lua
+
+--Init empty list of assets
+local assets = {}
+
+--Load all assets in the game
+assets.icon = love.graphics.newImage("assets/krecik_icon.jpg")
+
+--Return the list full of assets
+return assets
